224f5e2c7659480b72502392469ea83a526ad749
Stephen Beckstead <spotterbeckstead@gmail.com>
2023-10-20 21:06:48 -0600

    done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+    Random randomGenerator = new Random();
+    int sbnumber = randomGenerator.Next(1, 14);
+
+    int sbguess = 0; 
+
+    while (sbguess != sbnumber) {
+        Console.WriteLine("Enter Guess"); 
+        string sbrandom = Console.ReadLine();
+        sbguess = int.Parse(sbrandom); 
+
+        if (sbguess == sbnumber) 
+        {
+            Console.WriteLine("perfect");
+        }
+        if (sbguess > sbnumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        if (sbguess < sbnumber) 
+        {
+            Console.WriteLine("Higher");
+        }
+    }
+
+
     }
 }
